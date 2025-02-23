92c964939721f74a6a2043ddd41ebf3d7449e5fa
mehluli-series <mehluliseries@outlook.com>
2025-02-23 16:08:17 +0200

    Adding a rust data set with peoples names , ages and heights

--- linear_regression_model/src/main.rs
+++ linear_regression_model/src/main.rs
@@ -1,3 +1,22 @@
+struct Person {
+    name: String,
+    age: i32,
+    height: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let people: Vec<Person> = vec![
+        Person { name: "Mehluli".to_string(), age: 26, height: 173 },
+        Person { name: "Esihle".to_string(), age: 22, height: 154 },
+        Person { name: "Luyanda".to_string(), age: 22, height: 160 },
+        Person { name: "Ndumiso".to_string(), age: 35, height: 163 },
+        Person { name: "Belusiwe".to_string(), age: 28, height: 168 },
+        Person { name: "Karabo".to_string(), age: 40, height: 176 },
+        Person { name: "Lindiwe".to_string(), age: 32, height: 160 },
+    ];
+
+    // Print the dataset
+    for person in &people {
+        println!("Name: {}, Age: {}, Height: {} cm", person.name, person.age, person.height);
+    }
 }
